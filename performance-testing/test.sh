ae975a8a19eacb4f8aa4e4d3eca12dece6bae0c8
hotdog929 <hotdog929@gmail.com>
2021-11-25 13:53:51 +0800

    add performance testing report [sc-10802]

--- performance-testing/test.sh
+++ performance-testing/test.sh
@@ -3,14 +3,22 @@
 echo -e "*** Performance Testing ***\n" > performance-testing/report.txt
 
 echo -e "\n====== direct ======\n" >> performance-testing/report.txt
-sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8181:8181 --env QUARKUS_HTTP_PORT=8080 hotdog929/performance-testing-quarkus-native`
+sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8080:8080 hotdog929/performance-testing-quarkus-native`
+echo "= init =" >> performance-testing/report.txt
+groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
+sleep 1m
+echo "= warmup =" >> performance-testing/report.txt
 groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
 docker stop $sid
 sleep 1m
 
 echo -e "\n====== api-doorman-native ======\n" >> performance-testing/report.txt
-sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8181:8181 --env QUARKUS_HTTP_PORT=8181 hotdog929/performance-testing-quarkus-native`
-pid=`docker run --cpus="2" --memory="1g" --rm -d -p 8080:8080 api-doorman-native`
+sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8181:8080 hotdog929/performance-testing-quarkus-native`
+pid=`docker run --cpus="2" --memory="1g" --rm -d -p 8080:8080 -:Wq-add-host host.docker.internal:host-gateway hotdog929/api-doorman-native`
+echo "= init =" >> performance-testing/report.txt
+groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
+sleep 1m
+echo "= warmup =" >> performance-testing/report.txt
 groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
 docker stop $sid
 docker stop $pid
@@ -18,7 +26,11 @@ sleep 1m
 
 echo -e "\n====== nginx-proxy ======\n" >> performance-testing/report.txt
 sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8181:8181 --env QUARKUS_HTTP_PORT=8181 hotdog929/performance-testing-quarkus-native`
-pid=`docker run --cpus="2" --memory="1g" --rm -d -p 8080:8080 nginx-proxy`
+pid=`docker run --cpus="2" --memory="1g" --rm -d -p 8080:8080 --add-host host.docker.internal:host-gateway hotdog929/nginx-proxy`
+echo "= init =" >> performance-testing/report.txt
+groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
+sleep 1m
+echo "= warmup =" >> performance-testing/report.txt
 groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
 docker stop $sid
 docker stop $pid
