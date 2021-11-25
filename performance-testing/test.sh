9e54ba04d18caab8dcdcacff338576efc42239f6
hotdog929 <hotdog929@gmail.com>
2021-11-25 12:02:22 +0800

    fixed test.sh mistake [sc-10802]

--- performance-testing/test.sh
+++ performance-testing/test.sh
@@ -2,13 +2,13 @@
 
 echo -e "*** Performance Testing ***\n" > performance-testing/report.txt
 
-echo -e "\n====== no reverse proxy ======\n" >> performance-testing/report.txt
+echo -e "\n====== direct ======\n" >> performance-testing/report.txt
 sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8181:8181 --env QUARKUS_HTTP_PORT=8080 hotdog929/performance-testing-quarkus-native`
 groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
 docker stop $sid
 sleep 1m
 
-echo -e "\n====== api-doorman ======\n" >> performance-testing/report.txt
+echo -e "\n====== api-doorman-native ======\n" >> performance-testing/report.txt
 sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8181:8181 --env QUARKUS_HTTP_PORT=8181 hotdog929/performance-testing-quarkus-native`
 pid=`docker run --cpus="2" --memory="1g" --rm -d -p 8080:8080 api-doorman-native`
 groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
@@ -16,8 +16,9 @@ docker stop $sid
 docker stop $pid
 sleep 1m
 
-echo -e "\n====== nginx ======\n" >> performance-testing/report.txt
+echo -e "\n====== nginx-proxy ======\n" >> performance-testing/report.txt
 sid=`docker run --cpus="2" --memory="1g" --rm -d -p 8181:8181 --env QUARKUS_HTTP_PORT=8181 hotdog929/performance-testing-quarkus-native`
 pid=`docker run --cpus="2" --memory="1g" --rm -d -p 8080:8080 nginx-proxy`
+groovy performance-testing/tester.groovy >> performance-testing/report.txt 2>&1
 docker stop $sid
 docker stop $pid
